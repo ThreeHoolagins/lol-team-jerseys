c00db31d2c40bcd0f027c9f58ea5bbc7e79ec07c
ThreeHoolagins <cbcunningham20@gmail.com>
2025-01-02 22:57:24 -0600

    updated view, fixed everything, all in desprate need of a refactor

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lol-team-jerseys
+
+## Bug List
+
+- Champions like vel'koz are not being retrieved due to the special chacter, fix
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const championsJson = "https://ddragon.leagueoflegends.com/cdn/14.17.1/data/en_US/champion.json";
-const championJson = "https://ddragon.leagueoflegends.com/cdn/14.17.1/data/en_US/champion/";
+const championsJson = "https://ddragon.leagueoflegends.com/cdn/14.21.1/data/en_US/champion.json";
+const championJson = "https://ddragon.leagueoflegends.com/cdn/14.21.1/data/en_US/champion/";
 const gameVersion = "14.17.1";
 
 const getSkinSplashArtLink = (championName, skinNumber = 0) => {
@@ -28,44 +28,62 @@ function myFunction(value, index, array) {
     return index == value;
 }
 
+function toggleDiv (id) {
+    var divToToggle = document.getElementById(id);
+    divToToggle.hidden = !divToToggle.hidden;
+}
+
 const getViableLines = (map) => {
-    var returnList = []
     map.forEach((value, key) => {
-        if (value > 4) {
-            console.log(key)
-            returnList.push(key)
+        if (value.length < 5) {
+            map.delete(key)
         }
     });
-    return returnList;
+    printViable(map)
+}
+
+const printViable = (map) => {
+    var champsDiv = document.getElementById("champs");
+    map.forEach((value, key) => {
+        var setter = `\'${key}\'`
+        champsDiv.innerHTML += `<p class="centerText" onclick="toggleDiv(${setter})">${key} (${value.length})</p>`;
+        var imageGallery = `<div id=${setter} class="testClass" hidden>`
+        value.forEach(skinArr => {
+            var skinSplashLink = getSkinSplashArtLink(skinArr[1], skinArr[2]);
+            imageGallery += '<figure>'
+            imageGallery += `<img class="splash" src=\"${skinSplashLink}\">`
+            imageGallery += `<figcaption>${skinArr[0]}</figcaption>`
+            imageGallery += '</figure>'
+        })
+        imageGallery += (`</div>`);
+        champsDiv.innerHTML += imageGallery;
+    });
 }
 
 const getChampionNames = async () => {
     var call = await fetch(championsJson);
     var champList = await call.json();
-    var championDatas = champList.data;
-    console.log(championDatas);
-    var champsDiv = document.getElementById("champs");
+    var championDatas = Object.entries(champList.data);
     var champsJson = new Map();
-    Object.entries(championDatas).forEach(async championData => {
-        var championName = championData[1].name;
+    for (champDatainfo of championDatas) {
+        var championName = champDatainfo[1].id;
         let champjson = await getSkinLinesForChampion(championName);
-        champjson.forEach(value => {
-            if (value.name !== "default") {
-                var skinLine = value.name.replace(championName, "").trim();
+        console.log(typeof champjson);
+        Object.entries(champjson).forEach(value => {
+            if (value[1].name !== "default") {
+                var skinLine = value[1].name.replace(championName, "").trim();
                 if (!champsJson.has(skinLine)) {
-                    champsJson.set(skinLine, 1);
+                    champsJson.set(skinLine, [[value[1].name.trim(), championName, value[1].num]]);
                 }
                 else {
-                    champsJson.set(skinLine, champsJson.get(skinLine) + 1);
+                    champsJson.set(skinLine, [...champsJson.get(skinLine), [value[1].name.trim(), championName, value[1].num]]);
                 }
             }
         })
-        var viable = getViableLines(champsJson);
-        champsDiv.innerHTML = ("<p>" + viable + "</p>");
-    });
+    }
+    champsJson = new Map([...champsJson.entries()].sort());
+    getViableLines(champsJson);
     console.log(champsJson);
-
-
 }
 
-getChampionNames();
+window.onload = getChampionNames();
